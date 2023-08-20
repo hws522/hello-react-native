19bbdc7231b7499356b2338918a17c3387ae7cc2
hws522 <sraphic@kakao.com>
2023-08-20 16:31:06 +0900

    ~3.2 TextInput

--- TodoApp/App.js
+++ TodoApp/App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TouchableHighlight, TouchableWithoutFeedback, Pressable } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
 import { theme } from './colors';
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState('');
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const onChangeText = (payload) => setText(payload);
   return (
     <View style={styles.container}>
       <StatusBar style='auto' />
       <View style={styles.header}>
-        <TouchableOpacity>
-          <Text style={styles.btnText}>Work</Text>
+        <TouchableOpacity onPress={work}>
+          <Text style={{ ...styles.btnText, color: working ? 'white' : theme.grey }}>Work</Text>
         </TouchableOpacity>
-        <TouchableHighlight underlayColor={'red'} activeOpacity={0} onPress={() => console.log('pressed')}>
-          <Text style={styles.btnText}>Travel</Text>
-        </TouchableHighlight>
+        <TouchableOpacity onPress={travel}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: !working ? 'white' : theme.grey,
+            }}
+          >
+            Travel
+          </Text>
+        </TouchableOpacity>
       </View>
+      <TextInput keyboardType='default' onChangeText={onChangeText} value={text} placeholder={working ? 'Add a To Do' : 'Where do you want to go?'} style={styles.input} />
     </View>
   );
 }
@@ -33,6 +46,13 @@ const styles = StyleSheet.create({
   btnText: {
     fontSize: 38,
     fontWeight: '600',
-    color: 'white',
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 18,
   },
 });
